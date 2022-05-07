60b5e97a8638762e624febc1efcbf4200b6098c3
Evan Hazzard <ewhazzard@gmail.com>
2022-05-07 15:56:40 -0500

    Slight progress on mouse_map

--- mousejiggler/mouse_map.py
+++ mousejiggler/mouse_map.py
@@ -1,16 +1,23 @@
 #Mouse Map class that defines a map of mouse movements
 
+from threading import local
+
+
 class mouse_map:
     m_map = [0][0]
+    letter_maps = {
+        
+    }
 
     #Constructor for mouse_map class
     #Args: self, height, width
     #Returns: None
     def __init__(self, height, width):
-        m_map = [[0 for i in range(width)] for j in range(height)]
+        self.m_map = [[0 for i in range(width)] for j in range(height)]
         
     def get_map(self):
-        return m_map
+        return self.m_map
     
     def set_map(self, key):
-        pass      
+        local_map = [0][0]
+        self.letter_maps[key] = local_map
